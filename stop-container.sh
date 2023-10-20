6fb78e205aea2900b14abaaa02b15dcfab503d39
Ray <raymundo.salgado@alebateducation.com>
2023-10-20 14:59:09 -0600

    added fi

--- stop-container.sh
+++ stop-container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ \$(docker ps -q -f name=ieeecicd) ]; then 
+if [ \$(docker ps -q -f name=ieeecicd) ]; then
     docker stop ieeecicd
     docker rm ieeecicd
 else
     echo "El contenedor ieeecicd no está en ejecución"
+fi
